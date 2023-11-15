c20e534fcd019140cfd4ecc9695b8f08806029c5
Lubaba <tasniatushi97@gmail.com>
2023-11-15 11:38:52 +0600

    Submit java assignments

---
+++ src/assignments/Assignment5/DeptOfCse.java
@@ -0,0 +1,7 @@
+package assignments.Assignment5;
+
+public class DeptOfCse extends UndergradCourses {
+    public void cseCourses(){
+        System.out.println("Number of CSE courses : 25");
+    }
+}
---
+++ src/assignments/Assignment5/EmployeeTask4.java
@@ -0,0 +1,7 @@
+package assignments.Assignment5;
+
+public class EmployeeTask4 {
+    public void employeeStatus(){
+        System.out.println("Employee is permanent");
+    }
+}
